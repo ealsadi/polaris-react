cc7922a9a9cce157520b3ca4e681d8893cce16e9
Chloe Rice <chloe.rice@shopify.com>
2019-07-10 15:20:19 -0400

    pairing #2

--- src/components/Autocomplete/Autocomplete.tsx
+++ src/components/Autocomplete/Autocomplete.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import TextField from '../TextField';
+import Popover from '../Popover';
 
 export interface Props {
   /** A unique identifier for the Autocomplete */
@@ -15,6 +16,7 @@ export interface Props {
 export interface AutoCompleteContextType {
   allowMultiple?: boolean;
   loading?: boolean;
+  showEmptyState?: boolean;
   onScrolledToBottom?(): void;
   onSelect?(index: number): void;
 }
@@ -38,27 +40,70 @@ export default function Autocomplete({
     onSelect,
   };
 
+  const [active, setActive] = useState(false);
+
+  // const autoChildren = React.Children.map(children, (child => {
+  //   if (child.type === TextField) {
+  //     return React.cloneElement(child);
+  //   } else if (child.type === TabList) {
+  //     return React.cloneElement(child, {
+  //       activeIndex: this.state.activeIndex,
+  //       onActivateTab: this.handleTabActivate
+  //     });
+  //   } else {
+  //     return child;
+  //   }
+  // });
+
+  const autoTextField = React.Children && React.Children.toArray(children);
+  console.log(autoTextField);
+  // .filter(
+  //   (child: any) => child.type === TextField,
+  // )[0];
+
+  const autoDataList =
+    React.Children &&
+    React.Children.toArray(children).filter(
+      (child: any) => child.type === DataList,
+    )[0];
+
   return (
     <AutoCompleteContext.Provider value={context}>
+      <Popover
+        active={active}
+        activator={autoTextField as React.ReactElement}
+        onClose={() => setActive(false)}
+      >
+        {autoDataList}
+      </Popover>
       {children}
     </AutoCompleteContext.Provider>
   );
 }
 
 function DataList({children}: Props) {
-  return <React.Fragment>{children}</React.Fragment>;
+  return <ul>{children}</ul>;
 }
 
 function Title({children}: Props) {
-  return <React.Fragment>{children}</React.Fragment>;
+  return <li>{children}</li>;
 }
 
 function EmptyState({children}: Props) {
-  return <React.Fragment>{children}</React.Fragment>;
+  return <li>{children}</li>;
+}
+
+function Action({children}: Props) {
+  return <li>{children}</li>;
+}
+
+function Option({children}: Props) {
+  return <li>{children}</li>;
 }
 
 Autocomplete.TextField = TextField;
 Autocomplete.DataList = DataList;
+Autocomplete.Action = Action;
 Autocomplete.Option = Option;
 Autocomplete.Title = Title;
 Autocomplete.EmptyState = EmptyState;
